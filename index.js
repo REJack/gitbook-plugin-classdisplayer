aefc6b19886950b978ce4d1d9f5797324aba103d
REJack <info@rejack.de>
2016-05-15 16:05:22 +0200

    v1.0.0-pre3  - added any output is now escaped

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ module.exports = {
                     var class_name = parentBlock.args[0];
                     gl_class_name = class_name;
                     var class_desc = parentBlock.body.trim();
+                    var anchor = escape(class_name);
                     return '<div class="PHPclassDisplayer-class">'+
-                        '<a name="'+class_name+'"></a>'+
-                        '<div class="PHPcD-title">Class <b>'+class_name+'</b></div>'+
-                        '<div class="PHPcD-desc">' + class_desc + '<div>' +
+                        '<a name="'+anchor+'"></a>'+
+                        '<div class="PHPcD-title">Class <b>'+escape(class_name)+'</b></div>'+
+                        '<div class="PHPcD-desc">' + escape(class_desc) + '<div>' +
                     '</div>';
                 }
             }
@@ -43,16 +44,16 @@ module.exports = {
                 }
                 var const_name = parentBlock.args[0];
                 var const_desc = parentBlock.body.trim();
-                var anchor = const_name;
+                var anchor = escape(const_name);
                 var classSwitch = '';
                 if(gl_class_name !== ''){
-                    anchor = gl_class_name+'::'+const_name;
+                    anchor = escape(gl_class_name)+'::'+escape(const_name);
                     classSwitch = 'gl-';
                 }
                 return '<div class="PHPclassDisplayer-'+classSwitch+'const">'+
                     '<a name="'+anchor+'"></a>'+
-                    '<div class="PHPcD-title"><em>constant</em> <b>'+const_name+'</b></div>'+
-                    '<div class="PHPcD-desc">'+const_desc+'<div>'+
+                    '<div class="PHPcD-title"><em>constant</em> <b>'+escape(const_name)+'</b></div>'+
+                    '<div class="PHPcD-desc">'+escape(const_desc)+'<div>'+
                 '</div>';
             }
         },
@@ -64,6 +65,7 @@ module.exports = {
                     throw new Error('PHP class Displayer requires a argument as method/function-name');
                 }
                 var method_name = parentBlock.args[0];
+                method_name = escape(method_name);
                 method_name_wo_args = method_name.match(/(\w+)/);
                 method_name = method_name.replace(/(\w+)/, "<b>$1</b>");
                 method_name = method_name.replace(/([$]\w+)/g, "<em>$1</em>");
@@ -75,9 +77,9 @@ module.exports = {
                     if(block.name == 'param'){
                         param_count++;
                         method_params += '<li>'+
-                            '<b>'+block.args[0]+'</b> '+
-                            '(<em>'+block.kwargs.type+'</em>) '+
-                            '- '+block.body.trim()+
+                            '<b>'+escape(block.args[0])+'</b> '+
+                            '(<em>'+escape(block.kwargs.type)+'</em>) '+
+                            '- '+escape(block.body.trim())+
                         '</li>';
                     }else if(block.name == 'return'){
                         method_return = block.body.trim();
@@ -86,10 +88,10 @@ module.exports = {
                 method_params += '</ul>';
 
                 var classSwitch = 'function';
-                var anchor = method_name_wo_args[0]+'()';
+                var anchor = escape(method_name_wo_args[0])+'()';
                 if(gl_class_name !== ''){
                     classSwitch = 'method';
-                    anchor = gl_class_name+'::'+method_name_wo_args[0]+'()';
+                    anchor = escape(gl_class_name)+'::'+escape(method_name_wo_args)[0]+'()';
                 }
                 var params = '';
                 if(param_count !== 0){
@@ -104,8 +106,8 @@ module.exports = {
                 }
                 return '<div class="PHPclassDisplayer-'+classSwitch+'">' +
                     '<a name="'+anchor+'"></a>'+
-                    '<div class="PHPcD-title">' + method_name + '</div>' +
-                    '<div class="PHPcD-desc">' + method_desc + 
+                    '<div class="PHPcD-title">' + escape(method_name) + '</div>' +
+                    '<div class="PHPcD-desc">' + escape(method_desc) + 
                         '<table>' + 
                             params +
                             '<tr>' + 
@@ -113,7 +115,7 @@ module.exports = {
                                     '<b>Returns:</b>'+
                                 '</td>' +
                                 '<td class="PHPcD-return-text">' + 
-                                    method_return+
+                                    escape(method_return)+
                                 '</td>' +
                             '</tr>' +
                         '</table>' +
