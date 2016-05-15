4be18a2cee5a6bd2702fcea84171dee6496d3dd0
REJack <info@rejack.de>
2016-05-15 15:42:21 +0200

    v1.0.0-pre2  - added abilty to use method/function displayer without params  - added `width="20%"` to Parameters & Returns headline  - added Error messages if no argument is availbe (agrument is name)

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ module.exports = {
             process: function(parentBlock) {
                 if(parentBlock){
                     var blocks = [parentBlock].concat(parentBlock.blocks);
+                    console.log(parentBlock.args[0]);
+                    if (!parentBlock.args[0]) {
+                        throw new Error('PHP class Displayer requires a argument as class-name');
+                    }
                     var class_name = parentBlock.args[0];
                     gl_class_name = class_name;
                     var class_desc = parentBlock.body.trim();
@@ -34,6 +38,9 @@ module.exports = {
         PHPconstDisplayer: {
             process: function(parentBlock) {
                 var blocks = [parentBlock].concat(parentBlock.blocks);
+                if (!parentBlock.args[0]) {
+                    throw new Error('PHP class Displayer requires a argument as constant-name');
+                }
                 var const_name = parentBlock.args[0];
                 var const_desc = parentBlock.body.trim();
                 var anchor = const_name;
@@ -53,6 +60,9 @@ module.exports = {
             blocks: ['param','return'],
             process: function(parentBlock) {
                 var blocks = [parentBlock].concat(parentBlock.blocks);
+                if (!parentBlock.args[0]) {
+                    throw new Error('PHP class Displayer requires a argument as method/function-name');
+                }
                 var method_name = parentBlock.args[0];
                 method_name_wo_args = method_name.match(/(\w+)/);
                 method_name = method_name.replace(/(\w+)/, "<b>$1</b>");
@@ -60,8 +70,10 @@ module.exports = {
                 var method_desc = parentBlock.body.trim();
                 var method_params = '<ul>';
                 var method_return;
+                var param_count = 0;
                 blocks.forEach(function(block, i) {
                     if(block.name == 'param'){
+                        param_count++;
                         method_params += '<li>'+
                             '<b>'+block.args[0]+'</b> '+
                             '(<em>'+block.kwargs.type+'</em>) '+
@@ -79,21 +91,25 @@ module.exports = {
                     classSwitch = 'method';
                     anchor = gl_class_name+'::'+method_name_wo_args[0]+'()';
                 }
+                var params = '';
+                if(param_count !== 0){
+                    params = '<tr>' + 
+                        '<td width="20%" valign="top" class="PHPcD-params-title">' + 
+                            '<b>Parameters:</b>'+
+                        '</td>' +
+                        '<td class="PHPcD-params-list">' + 
+                            method_params+
+                        '</td>' +
+                    '</tr>';
+                }
                 return '<div class="PHPclassDisplayer-'+classSwitch+'">' +
                     '<a name="'+anchor+'"></a>'+
                     '<div class="PHPcD-title">' + method_name + '</div>' +
                     '<div class="PHPcD-desc">' + method_desc + 
                         '<table>' + 
+                            params +
                             '<tr>' + 
-                                '<td valign="top" class="PHPcD-params-title">' + 
-                                    '<b>Parameters:</b>'+
-                                '</td>' +
-                                '<td class="PHPcD-params-list">' + 
-                                    method_params+
-                                '</td>' +
-                            '</tr>' +
-                            '<tr>' + 
-                                '<td class="PHPcD-return-title">' + 
+                                '<td width="20%" class="PHPcD-return-title">' + 
                                     '<b>Returns:</b>'+
                                 '</td>' +
                                 '<td class="PHPcD-return-text">' + 
