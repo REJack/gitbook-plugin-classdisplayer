886de951fb14207aa39a9d8d943a38f89878ab4c
REJack <info@rejack.de>
2016-06-13 14:31:08 +0200

    v1.0.3-pre  - added `hint`-block for `PHPmethodDisplayer`  - added markdown abilty in `PHPmethodDisplayer` for `description`, `paramerter info` & `return body`  - some fixes with empty block in `PHPmethodDisplayer`

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var escape = require('escape-html');
+var markdown = require('gitbook-markdown');
 
 var gl_class_name = '';
 
@@ -65,8 +66,29 @@ module.exports = {
             }
         },
         PHPmethodDisplayer: {
-            blocks: ['param','return'],
+            blocks: ['hint','param','return'],
             process: function(parentBlock) {
+                var HINT_CLASSES = {
+                    info:'info',
+                    tip:'success',
+                    danger:'danger',
+                    working:'warning'
+                };
+
+                var hint_icon_info = this.config.options.pluginsConfig.phpclassdisplayer.hint_icon_info;
+                var hint_icon_tip = this.config.options.pluginsConfig.phpclassdisplayer.hint_icon_tip;
+                var hint_icon_danger = this.config.options.pluginsConfig.phpclassdisplayer.hint_icon_danger;
+                var hint_icon_working = this.config.options.pluginsConfig.phpclassdisplayer.hint_icon_working;
+                var hint_iconsize = this.config.options.pluginsConfig.phpclassdisplayer.hint_iconsize;
+                var hint_iconcenter = this.config.options.pluginsConfig.phpclassdisplayer.hint_iconcenter;
+
+                var HINT_ICONS = {
+                    info: '<i class="fa fa-'+hint_icon_info+' '+hint_iconsize+'"></i>',
+                    tip: '<i class="fa fa-'+hint_icon_tip+' '+hint_iconsize+'"></i>',
+                    danger: '<i class="fa fa-'+hint_icon_danger+' '+hint_iconsize+'"></i>',
+                    working: '<i class="fa fa-'+hint_icon_working+' '+hint_iconsize+'"></i>'
+                };
+
                 var blocks = [parentBlock].concat(parentBlock.blocks);
                 if (!parentBlock.args[0]) {
                     throw new Error('`PHPmethodDisplayer` requires a argument as method/function-name');
@@ -78,10 +100,33 @@ module.exports = {
                 method_name = method_name.replace(/([$]\w+)/g, "<em>"+escape("$1")+"</em>");
                 var method_desc = parentBlock.body.trim();
                 var method_params = '<ul>';
-                var method_return;
+                var method_return = '';
+                var method_hint = '';
                 var param_count = 0;
                 blocks.forEach(function(block, i) {
-                    if(block.name == 'param'){
+                    if(block.name == 'hint'){
+                        if(block.body.trim() !== ''){
+                            var style = block.kwargs.style || 'info';
+                            var hint_body = block.body.trim();
+                            var hint_center = '';
+                            if(hint_iconcenter === true){
+                                hint_center = 'align="middle"';
+                            }
+                            hint_body = markdown.page(hint_body).content;
+                            method_hint = '<div class="PHPcD-alert alert alert-'+HINT_CLASSES[style]+'">' + 
+                                '<table>' + 
+                                    '<tr>' + 
+                                        '<td width="15%" class="PHPcD-hint-icon" '+hint_center+'>' + 
+                                            '<b>'+HINT_ICONS[style]+'</b>'+
+                                        '</td>' +
+                                        '<td class="PHPcD-hint-text">' + 
+                                            hint_body.replace(/<p>|<\/p>/gi, '')+
+                                        '</td>' +
+                                    '</tr>' + 
+                                '</table>' +
+                            '</div>';
+                        }
+                    }else if(block.name == 'param'){
                         if (!block.args[0]) {
                             throw new Error('`PHPmethodDisplayer` requires for `param` a argument parameter-name');
                         }
@@ -89,17 +134,26 @@ module.exports = {
                             throw new Error('`PHPmethodDisplayer` requires for `param` a "type" property');
                         }
                         param_count++;
-                        var param_body = '';
-                        if (block.body.trim() !== '') {
-                            param_body = '- '+escape(block.body.trim());
-                        }
+                        var param_body = block.body.trim();
+                        param_body = markdown.page(param_body).content.replace(/<p>|<\/p>/gi, '');
                         method_params += '<li>'+
                             '<b>'+escape(block.args[0])+'</b> '+
                             '(<em>'+escape(block.kwargs.type)+'</em>) '+
-                            param_body+
+                            ' - '+param_body+
                         '</li>';
                     }else if(block.name == 'return'){
-                        method_return = block.body.trim();
+                        if(block.body.trim() !== ''){
+                            return_body = block.body.trim();
+                            return_body = markdown.page(return_body).content.replace(/<p>|<\/p>/gi, '');
+                            method_return = '<tr>' + 
+                                '<td width="20%" class="PHPcD-return-title">' + 
+                                    '<b>Returns:</b>'+
+                                '</td>' +
+                                '<td class="PHPcD-return-text">' + 
+                                    return_body+
+                                '</td>' +
+                            '</tr>';
+                        }
                     }
                 });
                 method_params += '</ul>';
@@ -129,18 +183,13 @@ module.exports = {
                 return '<div class="PHPclassDisplayer-'+classSwitch+'">' +
                     anchor_body+
                     '<div class="PHPcD-title">' + method_name + '</div>' +
-                    '<div class="PHPcD-desc">' + escape(method_desc) + 
+                    '<div class="PHPcD-desc">' + markdown.page(method_desc).content + 
                         '<table>' + 
                             params +
-                            '<tr>' + 
-                                '<td width="20%" class="PHPcD-return-title">' + 
-                                    '<b>Returns:</b>'+
-                                '</td>' +
-                                '<td class="PHPcD-return-text">' + 
-                                    escape(method_return)+
-                                '</td>' +
-                            '</tr>' +
+                            method_return +
                         '</table>' +
+                            method_hint +
+
                     '</div>' +
                 '</div>';
             }
